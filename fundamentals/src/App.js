8dad1875698d2d60a021577bf3ff862d8a942e43
Crystal <crystalyoodesign@gmail.com>
2023-03-01 13:49:03 +0900

    [fundamentals] create To-do example

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -1,8 +1,40 @@
+import { useState } from "react";
 import './css/App.css';
 
 function App() {
+  const [userInput, getUserInput] = useState("");
+  const getTodo = (e) => getUserInput(e.target.value);
+
+  const [todoArray, setTodoArray] = useState([]);
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    if (userInput === "") {
+      return;
+    }
+
+    setTodoArray((current) => [...current, userInput]);
+    getUserInput("");
+  };
+
   return (
     <div className="App">
+      <div className="container">
+        <section className="sectionTodo">
+          <h2>My To-Do List ({todoArray.length})</h2>
+          <form onSubmit={addTodo}>
+            <input
+              type="text"
+              value={userInput}
+              onChange={getTodo}
+            />
+            <button>Add</button>
+          </form>
+          <ul>
+            {todoArray.map((item, index) => <li key={index}>{item}</li>)}
+          </ul>
+        </section>
+      </div>
     </div>
   );
 }
